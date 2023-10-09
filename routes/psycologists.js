0ef9973ccecd0f651b40bbb1c9b8f6af924071f8
lishazzhr <lysha.sd08@gmail.com>
2023-10-10 02:12:19 +0700

    database

--- routes/psycologists.js
+++ routes/psycologists.js
@@ -9,6 +9,6 @@ import { verifyUser } from "../middleware/auth.js";
 const psycologistRouter = Router();
 
 psycologistRouter.get("/psycologists",verifyUser, getAllPsycologists);
-psycologistRouter.get("/psycologists/:id", getPsycologistsById);
+psycologistRouter.get("/psycologists/:id",verifyUser, getPsycologistsById);
 
 export default psycologistRouter;
